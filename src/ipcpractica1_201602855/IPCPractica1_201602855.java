524cb4c27f950ad9007d8b01222e0e724e9d2447
Alejo972017Marin <alejandrograndemarin@hotmail.com>
2017-08-23 23:27:07 -0600

    Listo

--- src/ipcpractica1_201602855/IPCPractica1_201602855.java
+++ src/ipcpractica1_201602855/IPCPractica1_201602855.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ipcpractica1_201602855;
 
 import java.io.BufferedReader;
@@ -10,10 +6,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-/**
- *
- * @author usuario
- */
 public class IPCPractica1_201602855 {
 
     
